2b5c6a4a243cb8c3919b9470a9e954144b4617f1
lafacemurilo <murilotap@hotmail.com>
2021-04-05 18:54:46 -0300

    retornando imagem em bytes

--- src/controllers/gerar-qrcode-estatico.js
+++ src/controllers/gerar-qrcode-estatico.js
@@ -1,4 +1,5 @@
 const qr_code_est = require('../actions/qr_code_estatico')
+const QRCode = require('qrcode')
 
 class gerar_qrcode_estatico {
 
@@ -14,8 +15,20 @@ class gerar_qrcode_estatico {
     //classe que contem o metodo que gera o qr code estatico
     const qr_code_estatico = new qr_code_est()
     const qr_code = await qr_code_estatico.gerar_qr_code_estatico(json)
+   
+   //gerar a imagem do qr code a partir do codigo do qr code
+    var qr_code_imagem = async qr_code => {
+      try {
+        qr_code_imagem = QRCode.toDataURL(qr_code)
+      } catch (err) {
+        console.error(err)
+      }
+    }
+    qr_code_imagem = await QRCode.toDataURL(qr_code);
 
-    return res.status(200).json({ qr_code })
+   
+
+    return res.status(200).json({'qr_code' : qr_code, 'qr_code_imagem' : qr_code_imagem })
 
   }
 
